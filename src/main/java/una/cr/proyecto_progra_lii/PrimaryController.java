7722b2c738a6d2efe6c2d709b480f1f4fda7f56c
Cristopher-MorenoUmana <ney.nmua@gmail.com>
2025-09-18 20:12:29 -0600

    initial commit

---
+++ src/main/java/una/cr/proyecto_progra_lii/PrimaryController.java
@@ -0,0 +1,12 @@
+package una.cr.proyecto_progra_lii;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ src/main/java/una/cr/proyecto_progra_lii/SecondaryController.java
@@ -0,0 +1,12 @@
+package una.cr.proyecto_progra_lii;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
